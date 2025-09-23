e91bcfafbaa8fef9c35e35208bd79701dd045f23
Ziad AlDohaim <ziada2@illinois.edu>
2025-09-23 12:22:06 -0500

    added sticky nav, nav recizing, layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
-/* Your JS here. */
-console.log('Hello World!')
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hello world");
+
+  //Scrolling:
+});
+
+window.onscroll = function () {
+  scroll();
+};
+function scroll() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    console.log("scroll");
+    document.getElementById("sticky").style.padding = "5px";
+    document.getElementById("stickyAnchor").style.padding = "5px";
+    document.getElementById("sticky").style.fontSize = "13px";
+  } else {
+    document.getElementById("sticky").style.padding = "20px";
+    document.getElementById("stickyAnchor").style.padding = "13px";
+    document.getElementById("sticky").style.fontSize = "18px";
+  }
+}
