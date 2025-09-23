4ccc408362121f9af92579b16990531e687add27
Ziad AlDohaim <ziada2@illinois.edu>
2025-09-23 13:52:55 -0500

    added multi column

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hello world");
-
-  //Scrolling:
 });
-
+//Scrolling:
 window.onscroll = function () {
   scroll();
 };
 function scroll() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    console.log("scroll");
     document.getElementById("sticky").style.padding = "5px";
     document.getElementById("stickyAnchor").style.padding = "5px";
     document.getElementById("sticky").style.fontSize = "13px";
@@ -18,4 +15,31 @@ function scroll() {
     document.getElementById("stickyAnchor").style.padding = "13px";
     document.getElementById("sticky").style.fontSize = "18px";
   }
+}
+//Carousel:
+let Index = 1;
+showSlides(Index);
+
+function moveSlides(n) {
+  showSlides((Index += n));
+}
+
+function currentSlide(n) {
+  showSlides((index = n));
+}
+
+function showSlides(n) {
+  let i = 0;
+  let slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+    Index = 1;
+  }
+  if (n < 1) {
+    Index = slides.length;
+    console.log(Index);
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[Index - 1].style.display = "block";
 }
